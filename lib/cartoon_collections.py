92a742555d9d3640a531b9bd3973d4fe4a163de5
Adam DePaolo <amdepaolo@gmail.com>
2023-10-09 18:42:36 -0400

    passes tests

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,20 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(names):
+    for index, name in enumerate(names):
+        print(f"{index + 1}. {name}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(list):
+    yelling_list = [word.capitalize() + "!" for word in list]
+    return yelling_list
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(foods):
+    cheeses = ["cheddar", "gouds", "camembert"]
+    for food in foods:
+        if food in cheeses:
+            return food
+    return None
